c4e19a758cf14434572c645f504778c1baf61d47
Felipe Serna <damsog38@gmail.com>
2021-06-19 21:22:36 -0500

    finished database script for basic functionalities

--- database/db.sql
+++ database/db.sql
@@ -1,24 +1,68 @@
-CREATE DATABASE database_links;
+CREATE DATABASE videoanalytics;
 
-USE database_links;
+USE videoanalytics;
 
--- PROFILES TABLE
-CREATE TABLE profiles (
-    id int(11) NOT NULL AUTO_INCREMENT,
-    type varchar(30) NOT NULL,
-    PRIMARY KEY id
-);
-
--- USERS TABLE
-CREATE TABLE users(
+-- ACCOUNTS TABLE
+CREATE TABLE accounts(
     id INT(11) NOT NULL AUTO_INCREMENT,
     username VARCHAR(16) NOT NULL,
     password VARCHAR(60) NOT NULL,
     fullname VARCHAR(100) NOT NULL,
     nick VARCHAR(60) NULL,
     logo VARCHAR(30) DEFAULT NULL,
-    profile_id INT(11) NOT NULL,
-    parent_prof INT(11) NOT NULL,
-    PRIMARY KEY id,
-    CONSTRAINT fk__user_profiles FOREIGN KEY (profile_id) REFERENCES profiles(id),
+    PRIMARY KEY (id)
 );
+
+-- PROFILES TABLE
+CREATE TABLE profiles(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    fullname VARCHAR(100) NOT NULL,
+    nick VARCHAR(60),
+    account_id INT(11) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk__profiles_accounts FOREIGN KEY (account_id) REFERENCES accounts(id)
+);
+
+-- GROUPS TABLE
+CREATE TABLE groups(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(16) NOT NULL,
+    dataset_route VARCHAR(60) NOT NULL,
+    account_id INT(11) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk__groups_accounts FOREIGN KEY (account_id) REFERENCES accounts(id)
+);
+
+-- CODERS TABLE
+CREATE TABLE coders(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    coder_img_route VARCHAR(60),
+    coder VARCHAR(60),
+    profile_id INT(11) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk__coders_profiles FOREIGN KEY (profile_id) REFERENCES profiles(id)
+);
+
+-- RELATIONS TABLE - PROFILES AND GROUPS
+CREATE TABLE relations(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    profile_id INT(11) NOT NULL,
+    group_id INT(11) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk__relations_profiles FOREIGN KEY (profile_id) REFERENCES profiles(id),
+    CONSTRAINT fk__relations_groups FOREIGN KEY (group_id) REFERENCES groups(id)
+);
+
+-- PERMITS TABLE 
+CREATE TABLE permits(
+    id INT(11) NOT NULL AUTO_INCREMENT,
+    account_grant_id INT(11) NOT NULL,
+    profile_grant_id INT(11) NOT NULL,
+    account_receive_id INT(11) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk__permits_accounts_grant FOREIGN KEY (account_grant_id) REFERENCES accounts(id),
+    CONSTRAINT fk__permits_profiles FOREIGN KEY (profile_grant_id) REFERENCES profiles(id),
+    CONSTRAINT fk__permits_accounts_receive FOREIGN KEY (account_receive_id) REFERENCES accounts(id)
+);
+
+
