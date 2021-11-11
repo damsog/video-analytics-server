d2a6a8271453fe337281b7e883b3bdab04c20362
kaiser24 <damsog38@gmail.com>
2021-11-10 21:49:07 -0500

    Finished adding routes for profiles

--- routes/profilesRoutes.js
+++ routes/profilesRoutes.js
@@ -1,2 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const profilesController = require("../controllers/profilesController");
+
+router.post('/', profilesController.createProfile);
+router.get('/', profilesController.getAllProfiles);
+router.get('/:id', profilesController.getProfileById);
+router.put('/:id', profilesController.updateProfileById);
+router.delete('/:id', profilesController.deleteProfileById);
