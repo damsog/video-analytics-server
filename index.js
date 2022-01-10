2d3900b1471de37f3983dfaf85f759bf6895a340
kaiser24 <damsog38@gmail.com>
2022-01-09 21:26:59 -0500

    Changed json limit to be 50mb to allow sending images as b64

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs)); // swag
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 // Global Variables
 
